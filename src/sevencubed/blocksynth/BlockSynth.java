dfbfb9c361c192a2a6505a3bb395c00cc613ea68
unknown <Skaught@.(none)>
2010-11-26 14:06:15 -0500

    dfsdfdsf

---
+++ src/sevencubed/blocksynth/BlockSynth.java
@@ -0,0 +1,13 @@
+package sevencubed.blocksynth;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BlockSynth extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
