29aaf77cfd645f940600a4cc30bad91019b96169
unknown <Skaught@.(none)>
2010-11-26 14:13:18 -0500

    cfdgdfgdfgdfg

--- src/sevencubed/blocksynth/BlockSynth.java
+++ src/sevencubed/blocksynth/BlockSynth.java
@@ -2,6 +2,8 @@ package sevencubed.blocksynth;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BlockSynth extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,12 @@ public class BlockSynth extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
     }
 }
